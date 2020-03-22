b371d9245f83b8e7235ca1dd0b01068ae57c3e99
Juliano Acuña <juliano.acunia@hotmail.com>
2020-03-22 20:02:49 -0300

    Create token middleware

--- app/functions/index.js
+++ app/functions/index.js
@@ -13,3 +13,27 @@ function createToken(user) {
     }
     return jwtSimple.encode(payload, config.token_key)
 }
+
+// THIS IS A TOKEN MIDDLEWARE
+function decodeToken(req, res, next) {
+    try{
+        console.log('HEADERS', req.headers)
+        const token = req.headers.authorization.replace('BEARER','')
+        const payload = jwtSimple.decode(token, config.token_key)
+        if(payload.exp <= moment().unix()){
+            return res
+            .status(401)
+            .send({ status: 401, messege: 'The token has expired'})
+        }
+    }
+    catch(err){
+        return res
+        .status(500)
+        .send({status: 500, messege: 'Incorrect token', err})
+    }
+}
+
+module.exports = {
+    createToken,
+    decodeToken
+}
